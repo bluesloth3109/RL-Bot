15cd7acaab60f21d638bb62ae6edb1d73c7f78fc
Callum Devlin <callumdevlin03@gmail.com>
2023-02-28 23:27:02 +0000

    sorted the flips out temp

--- RL Bot/src/bot.py
+++ RL Bot/src/bot.py
@@ -77,17 +77,17 @@ class WeeNanner(BaseAgent):
         if kickoff == True:
             controls.boost = True
             if 550 < car_velocity.length() < 650:
-                begin_speed_flip(packet)    
+                self.begin_speed_flip(packet)    
             if distance_to_ball < 460:
                 controls.boost = False
-                begin_front_flip(packet)
+                self.begin_front_flip(packet)
         
 
         #FRONT FLIP COOLDOWN TIMER 
         if car_location.dist(ball_location) <= 460 or 750 < car_velocity.length() < 800:
             if car_location[2] < 18.50:   
                 if curr_gametime - self.initial_gametime >= 4 :
-                    begin_front_flip(packet)
+                    self.begin_front_flip(packet)
                     self.initial_gametime = curr_gametime
 
         #do we need to powerslide to turn towards the ball
@@ -132,3 +132,45 @@ class WeeNanner(BaseAgent):
                     return True
                 else:
                     return False
+    
+    def begin_front_flip(self, packet):
+
+        # Send some quickchat just for fun
+        # Do a front flip. We will be committed to this for a few seconds and the bot will ignore other
+        # logic during that time because we are setting the active_sequence.
+        self.active_sequence = Sequence([
+            ControlStep(duration=0.05, controls=SimpleControllerState(jump=True)),
+            ControlStep(duration=0.05, controls=SimpleControllerState(jump=False)),
+            ControlStep(duration=0.2, controls=SimpleControllerState(jump=True, pitch=-1)),
+            ControlStep(duration=0.8, controls=SimpleControllerState()),
+        ])
+
+        # Return the controls associated with the beginning of the sequence so we can start right away.
+        return self.active_sequence.tick(packet)  
+
+    def begin_half_flip(self, packet):
+        self.active_sequence = Sequence([
+            ControlStep(duration=0.12, controls=SimpleControllerState(jump=True)),
+            ControlStep(duration=0.0001, controls=SimpleControllerState(jump=False)),
+            ControlStep(duration=0.09, controls=SimpleControllerState(jump=True, pitch=1)), #backflip
+            ControlStep(duration=0.4, controls=SimpleControllerState(jump=False, pitch=-1)), #cancel flip
+            #Adjust duration so it lands well
+            ControlStep(duration=1.4, controls=SimpleControllerState(jump=False, roll=1, boost=True)), #airrollround
+            ControlStep(duration=0.5, controls=SimpleControllerState(boost=True)),
+
+            ControlStep(duration=0.8, controls=SimpleControllerState()),
+
+        ])
+        return self.active_sequence.tick(packet)
+
+    def begin_speed_flip(self, packet):  
+        self.active_sequence = Sequence([
+            ControlStep(duration=0.15, controls=SimpleControllerState(steer=1, boost=True)),
+            ControlStep(duration=0.05, controls=SimpleControllerState(jump=True, boost=True)),
+            ControlStep(duration=0.05, controls=SimpleControllerState(jump=False, boost=True, handbrake=True)),
+            ControlStep(duration=0.2, controls=SimpleControllerState(jump=True, roll=-1, boost=True, handbrake=True)),
+            ControlStep(duration=0.15, controls=SimpleControllerState(jump=False, boost=True, handbrake=True)),
+            ControlStep(duration=0.40, controls=SimpleControllerState(boost=True, handbrake=True, yaw=-1)),
+        ])
+
+        return self.active_sequence.tick(packet)
